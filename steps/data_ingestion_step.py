1c4ef097a06d295c7e87e2a8354c9d80a95c3382
khouloud ouni <=>
2025-11-09 17:46:46 +0100

    feat: Implement feature engineering, missing value handling, model building, evaluation, and outlier detection steps
    - Added feature engineering strategies including log transformation, standard scaling, min-max scaling, and one-hot encoding.
    - Introduced missing value handling strategies for dropping and filling missing values.
    - Created model building strategy for linear regression with a pipeline.
    - Implemented model evaluation strategy using mean squared error and R-squared metrics.
    - Developed outlier detection strategies using Z-score and IQR methods.
    - Added steps for data ingestion, splitting, feature engineering, missing value handling, model building, evaluation, and outlier detection in the ZenML pipeline.
    - Included dynamic data importer and prediction service loader for model inference.
    - Enhanced logging throughout the steps for better traceability and debugging.

---
+++ steps/data_ingestion_step.py
@@ -0,0 +1,17 @@
+﻿import pandas as pd 
+from src.ingest_data import DataIngestionFactory
+from zenml import step
+
+@step
+def data_ingestion_step(file_path: str) -> pd.DataFrame:
+    """Ingest data from a specified file path using the appropriate data ingestor based on file extension.
+    Args:
+        file_path (str): The path to the data file.
+    Returns:
+        pd.DataFrame: The ingested data as a pandas DataFrame.
+    """
+    
+    file_extension = ".zip"
+    data_ingestor = DataIngestionFactory.get_data_ingestor(file_extension)
+    df = data_ingestor.ingest_data(file_path)
+    return df
